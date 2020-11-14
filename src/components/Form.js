091ff9708e0b438b2b8446c7d6f94c03a6ac387e
Polvyer <ocampowilliam27@gmail.com>
2020-11-13 22:29:57 -0600

    added babel support, which allows js files to be imported w/o being declared as an entry point

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <label>
+        <input type="text" />
+      </label>
+    </form>
+  );
+};
+
+export default Form;
--- src/register.js
+++ src/register.js
@@ -6,5 +6,9 @@
 import "./css/reset.css";
 import "./css/register.css";
 
-/* Images (import as many images as you need) */
-//import Chicago from "./images/chicago.png";
+/* React */
+import ReactDOM from "react-dom";
+import React from "react";
+import Form from "./components/Form.js";
+
+ReactDOM.render(<Form />, document.getElementById("root"));
