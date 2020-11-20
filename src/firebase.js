6149c57f01b10b9f91c4d878b6768d47af4add7e
Polvyer <ocampowilliam27@gmail.com>
2020-11-19 18:35:35 -0600

    added firebase and deployed initial

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,12 @@
-/* Firebase */
-import { f } from "./firebase";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
 
-/* CSS */
-import "./css/reset.css";
-import "./css/index.css";
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -16,3 +19,8 @@ const firebaseConfig = {
   appId: "1:448695709527:web:0317d5ce41565937311812",
   measurementId: "G-VRPVS0PHMV",
 };
+
+// Initialize Firebase
+var f = firebase.initializeApp(firebaseConfig);
+
+export { f };
--- src/register.js
+++ src/register.js
@@ -1,7 +1,3 @@
-/* Needed for Bootstrap (remove if you don't want to use bootstrap) */
-//import "bootstrap";
-//import "./scss/bootstrap.scss";
-
 /* CSS (import as many CSS files as you need) */
 import "./css/reset.css";
 import "./css/register.css";
