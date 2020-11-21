21706e3391e1719fac2de0601b4ead976de57e63
Eugenio Perez <eperez77@uic.edu>
2020-11-21 03:17:06 -0600

    datepage

--- src/date_select.js
+++ src/date_select.js
@@ -1,9 +1,14 @@
 /* Needed for Bootstrap (remove if you don't want to use bootstrap) */
-//import "bootstrap";
-//import "./scss/bootstrap.scss";
+import "bootstrap";
+import "./scss/bootstrap.scss";
 
 /* CSS (import as many CSS files as you need) */
 //import "./css/reset.css";
+import "./css/date_select.css";
 
 /* Images (import as many images as you need) */
 //import Chicago from "./images/chicago.png";
+
+$(document).ready(function () {
+  $("#js-date").datepicker();
+});
