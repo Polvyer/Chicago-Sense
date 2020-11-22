b75d715796f4a93cdd7a267f1900a62595118ac5
Polvyer <ocampowilliam27@gmail.com>
2020-11-22 14:26:46 -0600

    Added radial menu

--- src/activity.js
+++ src/activity.js
@@ -1,9 +1,137 @@
-/* Needed for Bootstrap (remove if you don't want to use bootstrap) */
-//import "bootstrap";
-//import "./scss/bootstrap.scss";
+/* CSS */
+import "./css/reset.css";
+import "./css/activity.css";
 
-/* CSS (import as many CSS files as you need) */
-//import "./css/reset.css";
+/* Images */
+import Group from "./images/group-icon.png";
+import Star from "./images/star.png";
+import Train from "./images/train.png";
+import Dollar from "./images/dollar.png";
 
-/* Images (import as many images as you need) */
-//import Chicago from "./images/chicago.png";
+/* SAVE FOR LATER FOR WHEN IMPLEMENTING POP UP BUBBLES
+//Add function to each main menu for show/hide sub menus
+var main1selected = true;
+wheel1.navItems[0].navigateFunction = function () {
+  if (!main1selected) {
+    wheel2.navItems[0].navItem.hide();
+    wheel2.navItems[1].navItem.hide();
+  } else {
+    wheel2.navItems[0].navItem.show();
+    wheel2.navItems[1].navItem.show();
+  }
+  main1selected = !main1selected;
+};
+
+var main2selected = true;
+wheel1.navItems[1].navigateFunction = function () {
+  if (!main2selected) {
+    wheel2.navItems[2].navItem.hide();
+    wheel2.navItems[3].navItem.hide();
+  } else {
+    wheel2.navItems[2].navItem.show();
+    wheel2.navItems[3].navItem.show();
+  }
+  main2selected = !main2selected;
+};
+
+var main3selected = true;
+wheel1.navItems[2].navigateFunction = function () {
+  if (!main3selected) {
+    wheel2.navItems[4].navItem.hide();
+    wheel2.navItems[5].navItem.hide();
+  } else {
+    wheel2.navItems[4].navItem.show();
+    wheel2.navItems[5].navItem.show();
+  }
+  main3selected = !main3selected;
+};
+*/
+
+const wheel = new wheelnav("wheelDiv");
+
+// DropMarker
+/*
+wheel.markerPathFunction = markerPath().DropMarker;
+wheel.markerAttr = { fill: "#333", stroke: "#333" };
+wheel.markerEnable = true;
+*/
+
+// Start wheelnav with no selection
+wheel.selectedNavItemIndex = null;
+
+// The colors of wheel defined by string array
+wheel.colors = ["#FFF"];
+
+// Enable keyboard navigation
+wheel.keynavigateEnabled = true;
+
+// The angle of wheel
+wheel.navAngle = 225;
+
+// The default wheelRadius is the half of the width of div
+wheel.wheelRadius = 180;
+
+// You can set the center of wheelnav
+wheel.centerX = 180;
+wheel.centerY = 180;
+
+// The default mode is rotation
+wheel.clickModeRotate = false;
+
+// Scale the icon size of SVGs
+wheel.sliceTransformFunction = sliceTransform().CustomTitleTransform;
+wheel.sliceTransformCustom = new sliceTransformCustomization();
+wheel.sliceTransformCustom.scaleString = "s2.25";
+
+// Scale the icon size of the SVG selected
+wheel.sliceSelectedTransformFunction = sliceTransform().CustomTitleTransform;
+wheel.sliceSelectedTransformCustom = new sliceTransformCustomization();
+wheel.sliceSelectedTransformCustom.scaleString = "s3.25";
+
+// The title of navItem can be text and icon. The icon can be svg or image.
+wheel.createWheel([
+  `imgsrc:${Group}`,
+  `imgsrc:${Dollar}`,
+  `imgsrc:${Star}`,
+  `imgsrc:${Train}`,
+]);
+
+// Every navItem element has an Attr property.
+wheel.slicePathAttr = { stroke: "#800000", "stroke-width": 3 };
+wheel.linePathAttr = { stroke: "#800000", "stroke-width": 2 };
+wheel.titleAttr = { fill: "#800000", stroke: "none" };
+
+// You can set attributes over hovered state.
+wheel.sliceHoverAttr = { stroke: "#00a1de", "stroke-width": 4 };
+wheel.lineHoverAttr = { stroke: "#00a1de", "stroke-width": 3 };
+wheel.titleHoverAttr = { fill: "#00a1de", stroke: "none" };
+
+// You can set attributes over selected state.
+wheel.sliceSelectedAttr = { stroke: "#9CF", "stroke-width": 4 };
+wheel.lineSelectedAttr = { stroke: "#9CF", "stroke-width": 4 };
+wheel.titleSelectedAttr = { fill: "#9CF" };
+
+wheel.refreshWheel();
+
+/* FAILED TEXT ATTEMPTS
+const path = document.querySelectorAll("path");
+console.log(path);
+
+//const span = document.createElement("span");
+//span.classList.add("tooltiptext");
+//span.textContent = "Hi";
+path[0].setAttribute("title", "hi");
+//path[0].appendChild(span);
+
+const images = document.querySelectorAll("image");
+console.log(images);
+images[0].setAttribute("title", "hi");
+
+images.forEach((image) => {
+  const span = document.createElement("span");
+  span.classList.add("tooltiptext");
+  span.textContent = "Hi";
+  image.classList.add("tooltip");
+  image.appendChild(span);
+});
+*/
